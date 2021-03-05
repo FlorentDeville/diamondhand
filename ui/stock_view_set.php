a38b04e86e8b5ae17b279eff7e39840833d553b2
florent.devillechabrol <tom.sawyer@outlook.com>
2021-03-04 21:21:53 -0800

    [ui] Add stock view per set.

--- ui/stock_view_set.php
+++ ui/stock_view_set.php
@@ -1,4 +1,14 @@
 <script>
+
+$(document).ready(function()
+	{
+		$("#set").change(function()
+		{
+			var set_id=$('#set').val();
+			window.location.href = "/index.php?page=stock_view_set.php&set_id=" + set_id;
+		});
+	});
+	
 	function delete_owned_card(owned_card_id)
 	{
 		var res = confirm('Are you sure you want to delete this card from your stock?')
@@ -22,8 +32,43 @@
 	}
 </script>
 <div>
+<div style="margin-bottom:20px;">
+	Select set :
+	<select name="set" id="set">
+		<?php
+			$sql = "select sets.id, sets.name, game.name as game_name from sets inner join game on sets.game_id=game.id;";
+			$result = $connection->query($sql);
+			
+			if(!isset($_GET["set_id"]))
+			{
+				echo "<option value=\"\" selected disabled hidden>Select Set</option>";
+			}
+			
+			while($row = $result->fetch())
+			{
+				if (isset($_GET["set_id"]))
+				{
+					$option = "";
+					$set_id = $_GET["set_id"];
+					if ($set_id == $row["id"])
+					{
+						$option = "selected";
+					}
+				}
+				echo "<option value=\"" . $row["id"] . "\"" . $option . ">(" . $row["game_name"] . ") " . $row["name"] . "</option>";
+			}
+		?>
+	</select>
+</div>
+	
 <?php
-	$sql = "SELECT round(sum(acq_price), 2) as money FROM owned_card";
+	if(!isset($_GET["set_id"]))
+	{
+		return;
+	}
+	$set_id = $_GET["set_id"];
+	
+	$sql = "SELECT round(sum(acq_price), 2) as money FROM owned_card inner join card on owned_card.card_id = card.id where card.set_id = " . $set_id;
 	$statement = $connection->query($sql);
 	$result = $statement->fetch();
 	$money = $result["money"];
@@ -33,16 +78,21 @@
 			owned_card.id as owned_card_id
 			from owned_card inner join card on owned_card.card_id=card.id inner join conditions on conditions.id=owned_card.condition_id
 			inner join sets on sets.id=card.set_id
-			inner join game on sets.game_id=game.id";
+			inner join game on sets.game_id=game.id
+			where card.set_id = " . $set_id;
 	$statement = $connection->query($sql);
 	if($statement == False)
 	{
 		echo "Failed to retrieve the owned card";
 	}
 	
+	$all_cards = $statement->fetchAll();
+	echo "<div> Found " . count($all_cards) . " cards.</div>";
+	
 	echo "<table>";
 	echo "<tr><th>Card Name</th><th>Set</th><th>Condition</th><th>Acq Price</th><th>Options</th></tr>";
-	while($result=$statement->fetch(PDO::FETCH_ASSOC))
+	//while($result=$statement->fetch(PDO::FETCH_ASSOC))
+	foreach($all_cards as $result)
 	{
 		if($result == False)
 		{
