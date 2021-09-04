b3392fba724418bbec8382e6421794b678ebfc72
florent.devillechabrol <tom.sawyer@outlook.com>
2021-09-04 16:52:05 -0700

    stock overview : add list of missing cards

--- ui/stock_overview.php
+++ ui/stock_overview.php
@@ -1,3 +1,36 @@
+<script>
+    function showMissingCard(set_id, clicked_element_id)
+    {
+        var sql = "select * from owned_card right join card on owned_card.card_id = card.id where owned_card.id is NULL and set_id=" + set_id;
+        console.log(sql);
+        $.get('php_scripts/execute_sql.php',{'sql':sql},function(return_data)
+        {
+            $("#missingCards").empty();
+            if(return_data.data.length>0)
+            {
+                console.log(return_data.data);
+                for(var ii = 0; ii < return_data.data.length; ++ii)
+                {
+                    var card = return_data.data[ii];
+                    $("#missingCards").append("<div>" + card.number + " " + card.name + "</div>");
+                }
+            }
+            else
+            {
+                $('#msg').html('No records Found');
+            }
+            
+            var oldSelectedSets = document.getElementsByClassName("progressionSelectedSet");
+            Array.prototype.forEach.call(oldSelectedSets, function(element) 
+            {
+                element.className = "progressionSet";
+            });
+            document.getElementById(clicked_element_id).className = "progressionSelectedSet";
+        }, "json");
+    }
+</script>
+<div style="display:flex;">
+    <div style="float:left; margin:0 50 0 0;">
 <?php
 
 $sql_get_all_sets = "select * from sets;";
@@ -16,13 +49,21 @@ while($row = $result->fetch())
     $owned_card_row = $owned_result->fetch();
     $owned_card = $owned_card_row[0];
 
+    $full_set = false;
     $style_name = "progressionSet";
     if($owned_card == $set_count)
     {
-        $style_name="progressionFullSet";
+        $full_set = true;
+        $style_name = "progressionFullSet";
     }
 
-    echo "<div class=\"".$style_name."\">";
+    $onclick = "";
+    if(!$full_set)
+    {
+        $onclick = "onclick=\"showMissingCard(".$set_id.", this.id)\"";
+    }
+
+    echo "<div class=\"".$style_name."\"".$onclick." id=\"".$set_id."\">";
     echo "<div style=\"text-align:center;\">" . $row["name"] . "</div>";
 
     echo "<div style=\"text-align:center;\">" . $owned_card . "/" . $set_count . "</div>";
@@ -36,5 +77,14 @@ while($row = $result->fetch())
     echo "<div style=\"float:left;height:".$HEIGHT."px;width:".$INCOMPLETE_SIZE.";background-color:red;\"></div>";
     echo "</div>";
     echo "</div>";
+
+    //select * from owned_card right join card on owned_card.card_id = card.id where set_id=9 and owned_card.id is NULL
 }
 ?>
+    </div>
+    <div id="missingCards">
+
+
+    </div>
+</div>
+<div id="msg"></div>
