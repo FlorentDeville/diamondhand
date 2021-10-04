11bc7a32fce4d97d6f734c665ce58836fe91e0a3
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-10-03 21:53:10 -0700

    Remove dead code.

--- ui/cards_set_list.php
+++ ui/cards_set_list.php
@@ -1,18 +1,3 @@
-<!-- <?php
-	$setId = $_GET["set_id"];
-	$query = $connection->query("select * from card where set_id = " . $setId . " order by number asc;");
-	echo "<table>";
-	echo "<tr><th>N</th><th>Name</th><th>Rarity</th><th>Variation</th><th>Links</th>";
-	while($row = $query->fetch())
-	{
-		echo "<tr><td>" . $row["number"] . "</td><td>" . $row["name"] . "</td><td>" . $row["rarity"] . "</td><td>" . $row["variation"] . "</td><td><a class='standard_link' href=\"" . $row["tcg_url"] . "\">TCGP</a></td></tr>";
-	}
-	echo "</table>";
-	$query = null;
-	$connection = null;
-	
-?> -->
-
 <script>
 $(document).ready(function()
 {
