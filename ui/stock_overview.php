e7387547bd8b467b118fcc6d71965c0854467951
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-10-12 22:12:22 -0700

    Fix stock overview for sets_langs table

--- ui/stock_overview.php
+++ ui/stock_overview.php
@@ -1,5 +1,5 @@
 <script>
-    function toggleShowMissingCard(set_id, clicked_element_id)
+    function toggleShowMissingCard(set_lang_id, clicked_element_id)
     {
         var SELECTED_CLASS_NAME = "progressionSelectedSet";
         var DEFAULT_CLASS_NAME = "progressionSet";
@@ -13,7 +13,7 @@
             return;
         }
 
-        var sql = "select * from owned_card right join card on owned_card.card_id = card.id where owned_card.id is NULL and set_id=" + set_id;
+        var sql = "select * from owned_card right join card on owned_card.card_id = card.id where owned_card.id is NULL and set_lang_id=" + set_lang_id;
         $.get('php_scripts/execute_sql.php',{'sql':sql},function(return_data)
         {
             //show missing cards
@@ -45,18 +45,19 @@
     <div style="float:left; margin:0 50 0 0;">
 <?php
 
-$sql_get_all_sets = "select * from sets;";
+$sql_get_all_sets = "select sets.name, sets_langs.id, languages.code 
+from sets_langs inner join sets on sets_langs.set_id = sets.id inner join languages on sets_langs.lang_id = languages.id;";
 $result = $connection->query($sql_get_all_sets);
 			
 while($row = $result->fetch())
 {
-    $set_id = $row["id"];
-    $sql_get_card_count = "select count(*) from card where set_id=" . $set_id;
+    $set_lang_id = $row["id"];
+    $sql_get_card_count = "select count(*) from card where set_lang_id=" . $set_lang_id;
     $set_count_result = $connection->query($sql_get_card_count);
     $set_count_row = $set_count_result->fetch();
     $set_count = $set_count_row[0];
 
-    $sql_get_owned_card_count = "select count(distinct card_id) from owned_card inner join card on owned_card.card_id = card.id where card.set_id =" . $set_id;
+    $sql_get_owned_card_count = "select count(distinct card_id) from owned_card inner join card on owned_card.card_id = card.id where card.set_lang_id =" . $set_lang_id;
     $owned_result = $connection->query($sql_get_owned_card_count);
     $owned_card_row = $owned_result->fetch();
     $owned_card = $owned_card_row[0];
@@ -72,11 +73,11 @@ while($row = $result->fetch())
     $onclick = "";
     if(!$full_set)
     {
-        $onclick = "onclick=\"toggleShowMissingCard(".$set_id.", this.id)\"";
+        $onclick = "onclick=\"toggleShowMissingCard(".$set_lang_id.", this.id)\"";
     }
 
-    echo "<div class=\"".$style_name."\"".$onclick." id=\"".$set_id."\">";
-    echo "<div style=\"text-align:center;\">" . $row["name"] . "</div>";
+    echo "<div class=\"".$style_name."\"".$onclick." id=\"".$set_lang_id."\">";
+    echo "<div style=\"text-align:center;\">" . $row["name"] . " (" . strtoupper($row["code"]) . ")</div>";
 
     echo "<div style=\"text-align:center;\">" . $owned_card . "/" . $set_count . "</div>";
 
