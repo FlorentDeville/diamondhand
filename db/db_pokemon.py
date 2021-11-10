175047ad9cc016d490417477a0884d2af412e91c
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-11-10 12:55:32 -0800

    Fix scrapping of number

--- db/db_pokemon.py
+++ db/db_pokemon.py
@@ -1,28 +1,40 @@
-import argparse
 import csv
+import logging
 import lxml.html
 import mysql.connector
+import os
+import re
 import sys
 import time
 import uuid
 
-from db.db_csv import DbCsv
-from db.entry import Entry
 from selenium import webdriver
 
-DB_TO_TCG_SET_NAME = {}
-DB_TO_TCG_SET_NAME["Base Set"] = "base-set"
+sys.path.append("../")
+from db.db_csv import DbCsv
+from db.entry import Entry
+
+GAMENAME = "Pokemon"
+
+logging.basicConfig(level=logging.INFO)
+rootLogger = logging.getLogger()
+
+logFilename = "c:/tmp/log_%s_%d.log" % (__name__, os.getpid())
+fileHandler = logging.FileHandler(logFilename)
+rootLogger.addHandler(fileHandler)
+
+log = logging.getLogger(__name__)
 
 
 class DbPokemon(DbCsv):
     m_browser = None
-    m_csv_filename = ""
+    m_csvFilename = ""
 
     def __init__(self, browser, csv_filename):
         self.m_browser = browser
-        self.m_csv_filename = csv_filename
+        self.m_csvFilename = csv_filename
 
-    def scrap_card_information(self, _url):
+    def scrap_card_information(self, _url, set_code):
         self.m_browser.get(_url)
         time.sleep(2)
         text_html = self.m_browser.execute_script("return document.body.innerHTML")
@@ -34,40 +46,52 @@ class DbPokemon(DbCsv):
         element = html.xpath(xpath_card_name)
         newEntry.name = element[0].text.strip()
 
-        xpath_set_name = "//h2[contains(@data-testid, 'lblProductDetailsSetName')]"
+        xpath_set_name = "//a[contains(@class, 'product-details__set-name')]/h2"
         element = html.xpath(xpath_set_name)
-        set_name = element[0].text
-        set_name = set_name.strip('\n').strip()
-        newEntry.set_name = set_name
-        newEntry.set_code = "i"  # make a global map of set name to set code
+        set_clean_name = element[0].text
+        set_clean_name = set_clean_name.strip('\n').strip()
+        newEntry.set_name = set_clean_name
+        newEntry.set_code = set_code
 
-        xpath_number_rarity = "//ul[contains(@class, 'product-details')]/li/span"
-        elements = html.xpath(xpath_number_rarity)
+        xpath_number = "//li/strong[contains(text(), \"Card Number / Rarity:\")]/following-sibling::span"
+        elements = html.xpath(xpath_number)
 
-        raw_text = elements[0].text
-
-        splitted = raw_text.split('/')
-        newEntry.number = splitted[0].strip()
-        newEntry.rarity = splitted[1].strip()
+        number_element = elements[0]
+        number_rarity = number_element.text
+        splitted = number_rarity.split("/")
+        if len(splitted) == 2:      # pattern is : 55 / Common
+            newEntry.number = splitted[0].strip()
+            newEntry.rarity = splitted[1].strip()
+        elif len(splitted) == 3:
+            pattern = "(\D)"
+            matches = re.match(pattern, splitted[0].strip())
+            if matches is None: # pattern is : 15/115 / Common
+                newEntry.number = splitted[0].strip()
+                newEntry.rarity = splitted[2].strip()
+            else:       # pattern is : Q/115 / Common
+                newEntry.number = splitted[0].strip() + splitted[1].strip()
+                newEntry.rarity = splitted[2].strip()
 
         newEntry.id = uuid.uuid4().int
         newEntry.tcg_url = _url
-        newEntry.variation = "Unlimited"
-
+        newEntry.variation = "None"
+        log.info("Found card %s", newEntry.name)
         return newEntry
 
     def write(self, _entry):
-        new_line = "{};{};{};{};{};{};{};{}\n"
+        new_line = "{};\"{}\";\"{}\";\"{}\";{};{};{};{}\n"
         new_line = new_line.format(_entry.id, _entry.set_name, _entry.set_code, _entry.name, _entry.number, _entry.rarity, _entry.tcg_url, _entry.variation)
-        with open(self.m_csv_filename, "a") as f:
+        with open(self.m_csvFilename, "a") as f:
             f.write(new_line)
 
     def load(self):
         entries = []
-        with open(self.m_csv_filename) as csvFile:
+        with open(self.m_csvFilename) as csvFile:
             lines = csv.reader(csvFile, delimiter=';', quotechar='"')
             for line in lines:
                 newEntry = Entry()
+                if len(line) == 0:  # empty line
+                    continue
                 newEntry.id = line[0]
                 newEntry.set_name = line[1]
                 newEntry.set_code = line[2]
@@ -76,82 +100,127 @@ class DbPokemon(DbCsv):
                 newEntry.rarity = line[5]
                 newEntry.tcg_url = line[6]
                 newEntry.variation = line[7]
+                if len(line) > 8 and line[8] == '1':
+                    newEntry.own = True
+                else:
+                    newEntry.own = False
                 entries.append(newEntry)
 
         return entries
 
 
-def scrap_all(setName):
-    print "Setup webdriver..."
+def scrap_all(set_input, csv_filename):
+    log.info("Setup webdriver...")
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument("--start-maximized")
     browser = webdriver.Chrome(executable_path="C:/workspace/python/chromedriver.exe", chrome_options=chromeOptions)
 
-    search_url_pattern = "https://www.tcgplayer.com/search/pokemon/{}?productLineName=pokemon&setName={}&ProductTypeName=Cards&page="
-    tcg_set_name = DB_TO_TCG_SET_NAME[setName]
-    search_url = search_url_pattern.format(tcg_set_name, tcg_set_name)
+    filename = csv_filename
+    db = DbPokemon(browser, filename)
 
-    db = DbPokemon(browser, "C:\\workspace\\python\\pokemon_urls\\db.csv")
+    search_url_pattern = set_input["url"]
 
-    page_count = 10
-    cards_collection = []
-    for page_id in range(1, page_count+1):
-        print "Scrap page " + str(page_id) + " of " + str(page_count) + "..."
-        complete_search_url = search_url + str(page_id)
+    page_count = set_input["page_count"]
+    for page_id in range(1, page_count + 1):
+        log.info("Scrap page %d of %d...", page_id, page_count)
+        complete_search_url = search_url_pattern.format(page_id)
 
         browser.get(complete_search_url)
         time.sleep(2)
         text_html = browser.execute_script("return document.body.innerHTML")
         html = lxml.html.fromstring(text_html)
 
-        xpath_cards = "//a[contains(@class, 'search-result__product')]"
+        xpath_cards = "//div[contains(@class, 'search-result__content')]/a"
         elements = html.xpath(xpath_cards)
 
         for element in elements:
             card_url = element.attrib.get('href')
+            card_url = "http://tcgplayer.com" + card_url
 
-            print card_url + "..."
-            entry = db.scrap_card_information(card_url)
+            log.info(card_url + "...")
+            entry = db.scrap_card_information(card_url, set_input["code"])
 
-            print entry.to_string()
-            cards_collection.append(entry)
+            log.info("writing to csv...")
+            db.write(entry)
 
-    print "Write to csv..."
-    for entry in cards_collection:
-        db.write(entry)
-
-    print "Over"
     browser.close()
 
 
-if __name__ == "__main__":
-    #parser = argparse.ArgumentParser(description="Parse tcgplayer to create the list of cards in a set of Pokemon.")
-    #parser.add_argument('--set-name', '-s', dest="setName", help="Name of the set to work with : Base Set")
-    #parser.add_argument("--scrap", dest="scrap", action="store_true", default=False, help="Scrap the data from tcgplayer.")
-    #parser.add_argument('--commit', '-c', dest="commit", action="store_true", default=False, help="Commit to the database.")
-    #options = parser.parse_args(sys.argv)
+def load_csv(csv_filename):
+    db = DbPokemon(None, csv_filename)
+    entries = db.load()
+    return entries
 
-    SCRAP = False
-    WRITE_IN_DB = True
-    COMMIT = True
 
-    if SCRAP is True:
-        set_name = "Base Set"
-        scrap_all(set_name)
-    elif WRITE_IN_DB is True:
-        db = DbPokemon(None, "C:\\workspace\\python\\pokemon_urls\\db.csv")
-        entries = db.load()
+# Push a set to the db and return its id
+def push_set_to_db(set_index, commit):
+    connection = mysql.connector.connect(host="localhost", user="root", database="wallstreet")
 
-        connection = mysql.connector.connect(host="localhost", user="root", database="wallstreet")
+    # find the game id
+    sql = "select id from game where game.name=%s"
+    sql_values = [GAMENAME]
+    cursor = connection.cursor()
+    cursor.execute(sql, sql_values)
+    results = cursor.fetchall()
+    if len(results) != 1:
+        print ("Error : Failed to find Pokemon in the game table")
+        return
 
-        setId = 23
-        for card in entries:
-            print card.name + "..."
-            cardInsertSql = "insert into card (name, set_id, rarity, variation, tcg_url, number) values (%s, %s, %s, %s, %s, %s)"
-            cardInsertSqlValues = [card.name, setId, card.rarity, card.variation, card.tcg_url, int(card.number)]
-            cursor = connection.cursor()
+    game_id = results[0][0]
+
+    set_info = sets[set_index]
+
+    #check if the set is already there
+    sql = "select id from sets where sets.name=%s and sets.game_id=%s"
+    sql_values = [set_info["clean_name"], game_id]
+    cursor = connection.cursor()
+    cursor.execute(sql, sql_values)
+    results = cursor.fetchall()
+    if len(results) > 0:
+        return results[0][0]  # the set already exists in the db
+
+    # add the set
+    sql = "insert into sets (name, code, release_date, game_id) values (%s, %s, %s, %s)"
+    sql_values = [set_info["clean_name"], set_info["code"], set_info["release_date"], game_id]
+    cursor = connection.cursor()
+    cursor.execute(sql, sql_values)
+    if commit:
+        connection.commit()
+
+    return cursor.lastrowid
+
+
+# Push all entries to the cards table
+def push_to_db(entries, set_id, variation, commit):
+    connection = mysql.connector.connect(host="localhost", user="root", database="wallstreet")
+    for card in entries:
+        cardInsertSql = "insert into card (name, set_id, rarity, variation, tcg_url, number) values (%s, %s, %s, %s, %s, %s)"
+
+        # extract the card number from the fftcg number which is <opus>-<number><rarity>
+        pattern = "\\d*-(\\d*)."
+        matches = re.match(pattern, card.number)
+        number = matches.group(1)
+        cardInsertSqlValues = [card.name, set_id, card.rarity, variation, card.tcg_url, int(number)]
+        cursor = connection.cursor()
+
+        # If the commit flag is false then we didn't push the set so the set_id doesn't exist.
+        # Executing the request will trigger a foreign key execption.
+        if commit:
             cursor.execute(cardInsertSql, cardInsertSqlValues)
-            if COMMIT:
-                connection.commit()
+            connection.commit()
 
-    print "Over"
+
+if __name__ == "__main__":
+    log.info("Setup webdriver...")
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_argument("--start-maximized")
+    browser = webdriver.Chrome(executable_path="C:/workspace/python/chromedriver.exe", chrome_options=chromeOptions)
+
+    filename = "C:\\workspace\\python\\data\\pokemon\\db_unseen-forces.csv"
+    db = DbPokemon(browser, filename)
+
+    url = "https://www.tcgplayer.com/product/90184/pokemon-unseen-forces-unown-q/before?xid=pi676a6c30-2663-4a95-8439-59e6360f8f2e&page=1&Language=English"
+    #url = "http://tcgplayer.com/product/85081/pokemon-unseen-forces-eevee?xid=pi3376190c-4835-4350-a9dd-14f41d33484d&page=1"
+    entry = db.scrap_card_information(url, "uf")
+
+    log.info("Over")
