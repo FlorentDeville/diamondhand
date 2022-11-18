b115cb2022cd10e00d049be773a0f41ec55374f9
florent.devillechabrol <florent.devillechabrol@gmail.com>
2022-11-18 10:29:38 -0800

    binder : filter by game and show result in table.

--- ui/binder.php
+++ ui/binder.php
@@ -1,36 +1,62 @@
-<!-- <div id="menu" style="margin:10 0 20 0;">
-    <div style="display:inline;"> Stocks : </div>
-    <a class="stockMenuButton" href="./index.php?page=binder.php&stock_page=pokemon_binder/side.html"><div class="stockMenuButton">Pokemon</div></a>
-    <a class="stockMenuButton" href="./index.php?page=binder.php&stock_page=stock_view.php"><div class="stockMenuButton">MTG</div></a>
-    <a class="stockMenuButton" href="./index.php?page=binder.php&stock_page=ff_binder/side.php"><div class="stockMenuButton">Final Fantasy</div></a>
-</div> -->
-<div id="content">
-    <?php
-        $sql = "select sets_langs.name as set_name, sets_langs.id as set_lang_id, game.id as game_id from sets_langs
+<script>
+    $(document).ready(function()
+	{
+        const queryString = window.location.search;
+	    const urlParams = new URLSearchParams(queryString);
+	    const game_id = urlParams.get('game_id');
+
+        var sql = `select sets_langs.name as set_name, sets_langs.id as set_lang_id, game.id as game_id
+        from sets_langs
         inner join sets on sets_langs.set_id = sets.id
-        inner join game on sets.game_id = game.id;";
-        $result = $connection->query($sql);
+        inner join game on sets.game_id = game.id
+        where game_id=` + game_id 
 
-        while($row = $result->fetch())
-        {
-            $MTG = 1;
-            $POK = 2;
-            $FF = 3;
+		$.get('php_scripts/execute_sql.php',{'sql':sql},function(return_data)
+		{
+			var sets = return_data.data;
 
-            $link_side = "";
-            $link_front = "";
-            switch($row["game_id"])
+            for(ii=0; ii < Object.keys(sets).length; ++ii)
             {
-                case $FF:
-                    $link_side = "./ff_binder/side.php?set_lang_id=" . $row["set_lang_id"];
-                    $link_front = "./ff_binder/front.php?set_lang_id=" . $row["set_lang_id"];
-                    break;
+                var card = sets[ii];
 
-                default:
-                break;
+                switch(game_id)
+                {
+                    case "1" : //mtg
+                        break;
+
+                    case "2" : //pokemon
+                        break;
+
+                    case "3" : //ff
+                        card["side_link"] = "./ff_binder/side.php?set_lang_id=" + card["set_lang_id"];
+                        card["front_link"] = "./ff_binder/front.php?set_lang_id=" + card["set_lang_id"];
+                        break;
+
+                    case "4" : //dbs
+                        break;
+
+                    default:
+                        break;
+                }
+                
             }
 
-            echo "<span>" . $row["set_name"] . "</span>&nbsp;<a target=\"_blank\" href=\"" . $link_side . "\">side</a>&nbsp; <a target=\"_blank\" href=\"" . $link_front . "\">front</a><br/>";
-        }
-    ?>
+			var column_array = new Array();
+            column_array.push({header_name:"Id", field_name:"set_lang_id", type:"int"});
+			column_array.push({header_name:"Set", field_name:"set_name", type:"string"});
+			column_array.push({header_name:"Side", field_name:"side_link", type:"link"});
+			column_array.push({header_name:"Front", field_name:"front_link", type:"link"});
+			display_table("content", column_array, sets, "set_lang_id", 0, "asc");
+		}, "json");
+	});
+</script>
+
+<div id="menu" style="margin:10 0 20 0;">
+    <div style="display:inline;"> Stocks : </div>
+    <a class="stockMenuButton" href="./index.php?page=binder.php&game_id=2"><div class="stockMenuButton">Pokemon</div></a>
+    <a class="stockMenuButton" href="./index.php?page=binder.php&game_id=1"><div class="stockMenuButton">MTG</div></a>
+    <a class="stockMenuButton" href="./index.php?page=binder.php&game_id=3"><div class="stockMenuButton">Final Fantasy</div></a>
+    <a class="stockMenuButton" href="./index.php?page=binder.php&game_id=4"><div class="stockMenuButton">DBS</div></a>
+</div>
+<div id="content">
 </div>
