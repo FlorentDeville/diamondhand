e8888e7550fc1e2a283e7342ac42e52373690017
florent.devillechabrol <florent.devillechabrol@gmail.com>
2022-12-17 23:52:46 -0800

    Show currently selected stock menu.

--- ui/stock_index.php
+++ ui/stock_index.php
@@ -1,11 +1,5 @@
-<div id="menu" style="margin:10 0 20 0;">
+<div id="stock_menu" style="margin:10 0 20 0;">
     <div style="display:inline;"> Stocks : </div>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_overview.php"><div class="stockMenuButton">Overview</div></a>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_overview_table.php"><div class="stockMenuButton">Table</div></a>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_view.php"><div class="stockMenuButton">All</div></a>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_view_set.php"><div class="stockMenuButton">Sets</div></a>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_add_card.php"><div class="stockMenuButton">Add single cards</div></a>
-    <a class="stockMenuButton" href="./index.php?page=stock_index.php&stock_page=stock_add_card_list.php"><div class="stockMenuButton">Add card lists</div></a>
 </div>
 <div id="content">
     <?php
@@ -17,3 +11,42 @@
         include("./" . $page);
     ?>
 </div>
+
+<script>
+    function addMenuButton()
+    {
+        const urlParams = new URLSearchParams(window.location.search);
+        const current_page = urlParams.get('stock_page');
+
+        menuList = [];
+        menuList.push({name:"Overview", link:"./index.php?page=stock_index.php&stock_page=stock_overview.php", id:"stock_overview.php"});
+        menuList.push({name:"Table", link:"./index.php?page=stock_index.php&stock_page=stock_overview_table.php", id:"stock_overview_table.php"});
+        menuList.push({name:"All", link:"./index.php?page=stock_index.php&stock_page=stock_view.php", id:"stock_view.php"});
+        menuList.push({name:"Sets", link:"./index.php?page=stock_index.php&stock_page=stock_view_set.php", id:"stock_view_set.php"});
+        menuList.push({name:"Add single cards", link:"./index.php?page=stock_index.php&stock_page=stock_add_card.php", id:"stock_add_card.php"});
+        menuList.push({name:"Add card lists", link:"./index.php?page=stock_index.php&stock_page=stock_add_card_list.php", id:"stock_add_card_list.php"});
+
+        var menu = document.getElementById("stock_menu");
+        for(var ii = 0; ii < menuList.length; ++ii)
+        {
+            item = menuList[ii];
+            var gameButton = document.createElement("a");
+            gameButton.href = item.link;
+            if(item.id == current_page)
+                gameButton.classList.add("selectedStockMenuButton");
+            else
+                gameButton.classList.add("stockMenuButton");
+
+            gameButton.style.padding="0px 2px 0px 2px";
+
+            var buttonContent = document.createElement("div");
+            buttonContent.textContent=item.name;
+            buttonContent.classList.add("stockMenuButton");
+            gameButton.appendChild(buttonContent);
+
+            menu.appendChild(gameButton);
+        }
+    }
+
+    addMenuButton();
+</script>
