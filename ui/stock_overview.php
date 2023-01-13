b7440dd55f4194213de1d6fe10ac84c6ee74adbb
florent.devillechabrol <florent.devillechabrol@gmail.com>
2023-01-12 20:14:43 -0800

    Add one piece to the stock overview.

--- ui/stock_overview.php
+++ ui/stock_overview.php
@@ -50,6 +50,7 @@
         gameList.push({name:"Pokemon", link:"./index.php?page=stock_index.php&stock_page=stock_overview.php&gameid=2", id:2});
         gameList.push({name:"FF", link:"./index.php?page=stock_index.php&stock_page=stock_overview.php&gameid=3", id:3});
         gameList.push({name:"DBS", link:"./index.php?page=stock_index.php&stock_page=stock_overview.php&gameid=4", id:4});
+        gameList.push({name:"OP", link:"./index.php?page=stock_index.php&stock_page=stock_overview.php&gameid=5", id:5});
 
         var menu = document.getElementById("game_menu");
         for(var ii = 0; ii < gameList.length; ++ii)
