ee491d3878429e9e58d4699b9a6c98f81ec3d4bb
florent.devillechabrol <florent.devillechabrol@gmail.com>
2023-03-10 20:16:06 -0800

    stock table : display the completion percentage.

--- ui/stock_overview_table.php
+++ ui/stock_overview_table.php
@@ -3,7 +3,7 @@
 	{
         var sql = `select count(distinct card_id) as owned_card, round(sum(acq_price), 2) as acq_price, card_count_table.card_count, 
         sets_langs.name as set_name, game.name as game_name,
-        sets_langs.id
+        sets_langs.id, count(distinct card_id)/card_count as percent_complete
         from owned_card inner join card on owned_card.card_id = card.id 
         inner join sets_langs on card.set_lang_id = sets_langs.id 
         inner join sets on sets.id = sets_langs.set_id 
@@ -21,7 +21,44 @@
 			column_array.push({header_name:"Count", field_name:"card_count", type:"int"});
 			column_array.push({header_name:"Owned", field_name:"owned_card", type:"int"});
 			column_array.push({header_name:"Acq Price", field_name:"acq_price", type:"float"});
+            column_array.push({header_name:"% Completion", field_name:"percent_complete", type:"float"});
 			display_table("table", column_array, sets, "id", 4, "desc");
+
+            var HEIGHT = 10;
+            var TOTAL_SIZE = 500;
+            containerElement = document.getElementById("table");
+            var children = containerElement.children[0].children;
+            for(var ii = 0; ii < children.length - 1; ++ii)
+            {
+                var childRowIndex = ii + 1;
+                var row = children[childRowIndex];
+                var percentCell = row.children[5];
+                var percent_complete = percentCell.textContent;
+
+                var cell = document.createElement("td");
+                row.appendChild(cell);
+                var COMPLETE_SIZE = percent_complete * TOTAL_SIZE;
+                var INCOMPLETE_SIZE = TOTAL_SIZE - COMPLETE_SIZE;
+
+                heightDiv = document.createElement("div");
+                heightDiv.style.height = HEIGHT + "px";
+                cell.appendChild(heightDiv);
+
+                completeDiv = document.createElement("div");
+                completeDiv.style.float = "left";
+                completeDiv.style.height = HEIGHT + "px";
+                completeDiv.style.width = COMPLETE_SIZE + "px";
+                completeDiv.style.backgroundColor = "green";
+                cell.appendChild(completeDiv);
+
+                incompleteDiv = document.createElement("div");
+                incompleteDiv.style.float = "left";
+                incompleteDiv.style.height = HEIGHT + "px";
+                incompleteDiv.style.width = INCOMPLETE_SIZE + "px";
+                incompleteDiv.style.backgroundColor = "red";
+                cell.appendChild(incompleteDiv);
+            }
+            
 		}, "json");
 	});
 
