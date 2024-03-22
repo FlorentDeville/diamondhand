4fc79dabeb2fa79cb99b1831e148d96e423af57e
Florent Deville <florent.devillechabrol@gmail.com>
2024-03-22 14:18:01 -0700

    Update db backup script with new output folder and mysql version.

--- db_backup/backup.bat
+++ db_backup/backup.bat
@@ -1,5 +1,5 @@
 @echo off
-set MYSQL_ROOT=C:\wamp64\bin\mysql\mysql5.7.31\bin
+set MYSQL_ROOT=C:\wamp64\bin\mysql\mysql8.2.0\bin
 
 set DB_NAME=wallstreet
 
@@ -8,7 +8,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%b-%%a-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
 set DATETIME=%mydate%_%mytime%
 
-set OUTPUT=C:\card\db_backup\%DB_NAME%_backup_%DATETIME%.sql
+set OUTPUT=C:\Workspace\diamondhand\db_backup\history\%DB_NAME%_backup_%DATETIME%.sql
 
 %MYSQL_ROOT%\mysqldump.exe --databases %DB_NAME% --result-file %OUTPUT% --user root
 
