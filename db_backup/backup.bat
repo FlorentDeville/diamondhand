72d7e2124228586d4b5ab62346aa213a04d8a265
Florent Deville <florent.devillechabrol@gmail.com>
2024-05-18 09:20:06 -0700

    Fix date format in back up sql script so the file names can be sorted by date

--- db_backup/backup.bat
+++ db_backup/backup.bat
@@ -4,8 +4,9 @@ set MYSQL_ROOT=C:\wamp64\bin\mysql\mysql8.2.0\bin
 set DB_NAME=wallstreet
 
 REM Get current date
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%b-%%a-%%c)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (if %%a LSS 10 (set mytime=0%%a-%%b) else (set mytime=%%a-%%b))
+
 set DATETIME=%mydate%_%mytime%
 
 set OUTPUT=C:\Workspace\diamondhand\db_backup\history\%DB_NAME%_backup_%DATETIME%.sql
